4e38a139273966fdb60d62600df200f7f6310f1d
Brian Smith <block8437@gmail.com>
2017-03-24 09:18:41 -0400

    Added in vasa_merge

--- src/arch/i386/mem/vasa.c
+++ src/arch/i386/mem/vasa.c
@@ -15,20 +15,39 @@ void vasa_add_node(vasa_node_t* node, bool used) {
         return;
     }
 
+    vasa_node_t* prev = NULL;
     vasa_node_t* head = used ? global_asa.used_head : global_asa.free_head;
 
     while ( head ) {
-        if ( (uintptr_t) node->base > (uintptr_t) head->base ) {
-            node->next = head->next;
-            head->next = node;
+        // Add the node into the first place before where the head's base is greater than us.
+        if ( (uintptr_t) head->base > (uintptr_t) node->base ) {
+            if ( prev ) {
+                // Add it inline
+                prev->next = node;
+                node->next = head;
+            }
+            else {
+                // Nothing before, add it in the beginning
+                node->next = head;
+
+                if ( used ) {
+                    global_asa.used_head = node;
+                }
+                else {
+                    global_asa.free_head = node;
+                }
+            }
+
 
             return;
         }
 
+        prev = head;
         head = head->next;
     }
 
     if ( head == NULL ) {
+        // No head, just add it in.
         if ( used ) {
             global_asa.used_head = node;
         }
@@ -37,9 +56,38 @@ void vasa_add_node(vasa_node_t* node, bool used) {
         }
     }
     else {
+        // Ran to the end, add to the end.
         head->next = node;
     }
 }
+
+// Take the free or used lists and merge the chunks of memory if they're continous
+void vasa_merge(bool used) {
+    vasa_node_t* head;
+
+    if ( used ) {
+        head = global_asa.used_head;
+    }
+    else {
+        head = global_asa.free_head;
+    }
+
+    while ( head != NULL && head->next != NULL ) {
+        // Is the node continous?
+        if ( head->base + head->length == head->next->base ) {
+            // Merge the nodes and correct the linked list.
+            vasa_node_t* mergeable = head->next;
+            head->length += head->next->length;
+            head->next = head->next->next;
+            kfree(mergeable);
+
+            continue;
+        }
+
+        head = head->next;
+    }
+}
+
 
 // Allocate virtual address space for a given memory type
 void* vasa_alloc(vasa_memtype_t type, unsigned long size) {
